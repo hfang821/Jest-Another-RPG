259e288c7c255892fd7393c84527fbaaf6238b7d
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-21 17:29:04 -0400

    finished the player constructor
    adding the addAttackValue(), addPotion(), usePotion()

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,3 +65,28 @@ test("subtracts from player's health", () =>{
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
 });
+
+test("get player's attack value", ()=>{
+    const player = new Player('Dave');
+    player.length = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('add a potion to the inventory', ()=>{
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+    //not using the actual potion, it would no longer be an isolated unit test
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('use a potion from inventory', ()=>{
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+    expect(player.inventory.length).toBeLessThan(oldCount);
+})
